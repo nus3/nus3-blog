2b374239f8bfc9a0e7e7a5ad783ef763eb812569
nus3 <hadayota33@gmail.com>
2021-09-28 00:18:37 +0900

    :wrench: Add react icon

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 // HACK:(nus3) Reactをimportしたくない
 import * as React from 'react'
 import { PageProps, graphql } from 'gatsby'
+import { FaTwitter, FaGithub } from 'react-icons/fa'
 
 import { Layout } from '../components/Layout'
 import Seo from '../components/seo'
@@ -14,10 +15,22 @@ const BlogTop: React.FC<PageProps<GatsbyTypes.BlogTopQuery>> = ({
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="Top" />
+      {/* TODO:(nus3) コンポーネント化 */}
       <ul>
         <li>zenn 技術的なことはこっち</li>
         <li>blogs 個人的なことはこっち</li>
       </ul>
+      {/* TODO:(nus3) コンポーネント化 */}
+      <section className="flex gap-3 items-center">
+        <FaTwitter
+          className="text-accentYellow text-5xl"
+          title="Twitterのアイコン"
+        ></FaTwitter>
+        <FaGithub
+          className="text-accentYellow text-5xl"
+          title="GitHubのアイコン"
+        ></FaGithub>
+      </section>
     </Layout>
   )
 }
