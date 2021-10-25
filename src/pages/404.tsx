9f88ff38004cd89d7a24bea7cb48bad33f641726
nus3 <hadayota33@gmail.com>
2021-10-25 23:16:28 +0900

    :art: Add style in 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,8 +1,9 @@
 import * as React from 'react'
-import { graphql, PageProps } from 'gatsby'
+import { graphql, PageProps, Link } from 'gatsby'
 
 import { Layout } from '../components/Layout'
 import Seo from '../components/seo'
+import { FaArrowCircleLeft } from 'react-icons/fa'
 
 const NotFoundPage: React.FC<PageProps<GatsbyTypes.NotFoundQuery>> = ({
   data,
@@ -13,8 +14,17 @@ const NotFoundPage: React.FC<PageProps<GatsbyTypes.NotFoundQuery>> = ({
   return (
     <Layout location={location} title={siteTitle}>
       <Seo title="404: Not Found" />
-      <h1>404: Not Found</h1>
-      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+      <section className="flex flex-col gap-5 justify-center items-center h-full w-full">
+        <h1 className="font-bold text-accentYellow text-9xl">404</h1>
+        <p className="text-xl">{`ページがみつかんない _:(´ཀ\`」 ∠):`}</p>
+        <Link
+          to="/blogs"
+          className="flex gap-1 items-center text-primary hover:text-primaryLight transition-colors duration-300"
+        >
+          <FaArrowCircleLeft className="text-lg" />
+          <span>記事一覧に戻る</span>
+        </Link>
+      </section>
     </Layout>
   )
 }
