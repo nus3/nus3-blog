27b9a7c7a0bf8fdbfe29796b95a17a8bf2894fad
nus3 <hadayota33@gmail.com>
2023-02-26 14:27:49 +0900

    :wrench: add react integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,15 @@
-import { defineConfig } from 'astro/config'
-import tailwind from '@astrojs/tailwind'
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'static',
   markdown: {
-    syntaxHighlight: 'prism',
+    syntaxHighlight: 'prism'
   },
   site: 'https://nus3.com/',
-  integrations: [tailwind()],
-})
+  integrations: [tailwind(), react()]
+});
